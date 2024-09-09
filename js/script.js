a3e8d16f2695da074f6e6521880d430218046868
Khansa-21 <khansaehsan21@gmail.com>
2024-09-09 20:22:31 +0100

    2nd FORM

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+function print() {
+    var username = document.getElementById('user').value
+    var password = document.getElementById('pass').value
+    console.log('Username:', username)
+    console.log('Password:', password)
+}
